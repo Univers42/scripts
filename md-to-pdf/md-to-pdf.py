a86c0d929d0508d9cce507949c3274d6bbfe36a4
LESdylan <dev.pro.photo@gmail.com>
2026-05-28 22:27:39 +0200

    update

--- md-to-pdf/md-to-pdf.py
+++ md-to-pdf/md-to-pdf.py
@@ -1,20 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    md-to-pdf.py                                       :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-<<<<<<< HEAD
-#    Created: 2026/05/18 21:19:26 by dlesieur          #+#    #+#              #
-#    Updated: 2026/05/18 21:19:26 by dlesieur         ###   ########.fr        #
-=======
-#    Created: 2026/05/18 21:19:31 by dlesieur          #+#    #+#              #
-#    Updated: 2026/05/18 21:19:31 by dlesieur         ###   ########.fr        #
->>>>>>> tmp/detached-a3c30f43
-#                                                                              #
-# **************************************************************************** #
-
 #!/usr/bin/env python3
 """md-to-pdf.py — Markdown + Mermaid to Professional PDF (WeasyPrint + Kroki).
 
